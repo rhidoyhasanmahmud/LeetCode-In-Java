d40b7bb83030378be27b5b7930242bf988d87e64
Hasan Mahmud <hasan.mahmud8177@gmail.com>
2022-01-19 00:05:11 +0600

    Solved -> '1385 - Find the Distance Value Between Two Arrays'

--- src/main/java/LeetCode_1385.java
+++ src/main/java/LeetCode_1385.java
@@ -1,27 +1,21 @@
 public class LeetCode_1385 {
     public static void main(String[] args) {
-        System.out.println(findTheDistanceValue(new int[]{4, 5, 8}, new int[]{10, 9, 1, 8}, 2));
+        System.out.println(findTheDistanceValue(new int[]{2,1,100,3}, new int[]{-5,-2,10,-3,7}, 6));
     }
 
     public static int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
         int totalNotMatchCodition = 0;
         boolean isConditionNotMatch = false;
 
-        for (int k = 0; k < arr1.length; k++) {
-            for (int i = 0; i < arr2.length; i++) {
-                System.out.println(arr1[k] +" "+ arr2[i] + " " + d + " " + Math.abs(arr1[k] - arr2[i]));
-                System.out.println(Math.abs(arr1[k] - arr2[i])<=d);
-                if ((Math.abs(arr1[k] - arr2[i]) <= d)) {
-                    System.out.println("OK");
+        for (int j : arr1) {
+            for (int k : arr2) {
+                if ((Math.abs(j - k)) > d) {
                     isConditionNotMatch = true;
                 } else {
                     isConditionNotMatch = false;
                     break;
                 }
-
             }
-            System.out.println("----------------");
-            System.out.println(isConditionNotMatch);
             if (isConditionNotMatch) {
                 totalNotMatchCodition++;
             }
