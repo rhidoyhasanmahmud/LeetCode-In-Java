51ea4c649a43bf44b2ae5cb44fd98d2dffbb6fa9
Hasan Mahmud <hasan.mahmud8177@gmail.com>
2022-01-31 23:43:24 +0600

    Solved -> '1672. Richest Customer Wealth'

--- src/main/java/LeetCode_1672.java
+++ src/main/java/LeetCode_1672.java
@@ -1,27 +1,22 @@
 public class LeetCode_1672 {
     public static void main(String[] args) {
         int distances[][] = {
-                {1, 5},
-                {7, 3},
-                {3, 5}
+                {2, 8, 7}, {7, 1, 3}, {1, 9, 5}
         };
         System.out.println(maximumWealth(distances));
     }
 
     public static int maximumWealth(int[][] accounts) {
-        int richestWealth = 0;
+        int sum = 0;
         int len = accounts.length;
-
         for (int i = 0; i < len; i++) {
-            int sum = 0;
-            int yDimensionLen = accounts[i].length;
-            for (int j = 0; j < yDimensionLen; j++) {
-                sum += accounts[i][j];
+            int totalSum = 0;
+            int innerLen = accounts[i].length;
+            for (int j = 0; j < innerLen; j++) {
+                totalSum += accounts[i][j];
             }
-            if (sum > richestWealth) {
-                richestWealth = sum;
-            }
+            sum = Math.max(sum, totalSum);
         }
-        return richestWealth;
+        return sum;
     }
 }
